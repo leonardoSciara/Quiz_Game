700f13607c3f6d92a7db571202936908dc5a4efb
M318 <leonardo.sciara@samtrevano.ch>
2024-10-09 15:36:55 +0200

    Creazione Pagina Username

---
+++ 5_Applicativo/Quiz_Game/src/quiz_game/Quiz_Game.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quiz_game;
+
+/**
+ *
+ * @author leonardo.sciara
+ */
+public class Quiz_Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
